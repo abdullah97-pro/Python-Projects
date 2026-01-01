4f2cd2ac45560eed9591447d9d64ffe973a5ddc4
abdullah97-pro <abdullahahmadi019@gmail.com>
2026-01-01 13:56:27 +0430

    variable and datatype

--- file1.py
+++ file1.py
@@ -1,1 +1,11 @@
-print("First line")
+firstName = "Ali"
+age = 21
+marital_status = True
+fee = 50
+dob = "2005-05-20"
+
+print("FirstName: ",firstName)
+print("Age: ",age)
+print("Maried: ",marital_status)
+print("Fee: ",fee)
+print("DOB: ",dob)
