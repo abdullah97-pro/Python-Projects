db4654bcc58566fda42ca85f2dec3359577828c5
abdullah97-pro <abdullahahmadi019@gmail.com>
2026-01-01 13:58:11 +0430

    basic math operator

--- file1.py
+++ file1.py
@@ -1,11 +1,16 @@
-firstName = "Ali"
-age = 21
-marital_status = True
-fee = 50
-dob = "2005-05-20"
+# firstName = "Ali"
+# age = 21
+# marital_status = True
+# fee = 50
+# dob = "2005-05-20"
 
-print("FirstName: ",firstName)
-print("Age: ",age)
-print("Maried: ",marital_status)
-print("Fee: ",fee)
-print("DOB: ",dob)
+# print("FirstName: ",firstName)
+# print("Age: ",age)
+# print("Maried: ",marital_status)
+# print("Fee: ",fee)
+# print("DOB: ",dob)
+
+num1 = 12
+num2 = 3
+result = num1 * num2
+print(result)
