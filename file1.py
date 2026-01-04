8b86f3f99d383c608b64fea7b9163788f22942cd
abdullah97-pro <abdullahahmadi019@gmail.com>
2026-01-04 13:32:02 +0430

    controll flow

--- file1.py
+++ file1.py
@@ -10,9 +10,42 @@
 # print("Fee: ",fee)
 # print("DOB: ",dob)
 
-name = input("Enter your name: ")
-num1 = int(input("Enter number 1: "))
-num2 = int(input("Enter number 2: "))
-result = num1 * num2
-print(name)
-print(result)
+# name = input("Enter your name: ")
+# num1 = int(input("Enter number 1: "))
+# num2 = int(input("Enter number 2: "))
+# result = num1 * num2
+# print(name)
+# print(result)
+
+# x = 12
+# y = 3
+
+# result = x + y
+
+# print(result)
+# if result == 15:
+#     print("Fifteen")
+
+# if result != 15:
+#     print("No Fifteen")
+
+# if result > 14:
+#     print("Greater then 14")
+
+# if result != 15:
+#     print(0)
+# else:
+#     print(1)
+
+areaCode = int(input("Enter Code: "))
+
+if areaCode == 90:
+    print("Turkey")
+elif areaCode == 91:
+    print("India")
+elif areaCode == 92:
+    print("Pakistan")
+elif areaCode == 93:
+    print("Afghanistan")
+else:
+    print("Try again !!!")
