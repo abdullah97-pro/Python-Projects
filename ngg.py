f110dbf5218d70fbe274b1cce3d2bb37f1ded747
abdullah97-pro <abdullahahmadi019@gmail.com>
2026-01-05 13:56:36 +0430

    Number Guessing Game 2

--- ngg.py
+++ ngg.py
@@ -15,5 +15,31 @@ def number_guessing_game():
     print("\n Choose your difficulty level:")
     for key, value in difficulty_levels.items():
         print(f"{key}, {value['name']} - {value['attempts']} attempts")
+    
+    while True:
+        choice = input("\nEnter your choice (1-3):")
+
+        # check if choice is valid 
+        if choice in ["1","2","3"]:
+            min_num, max_num = difficulty_levels[choice]["range"]
+            max_attempts = difficulty_levels[choice]["attempts"]
+            difficulty_name = difficulty_levels[choice]["name"]
+            break
+        else:
+            print("Invalid choice! Please enter 1,2,3")
+
+    # Generate random number
+    secret_number = random.randint(min_num,max_num)
+
+    print(f"\nI'm thinking of a number between {min_num} and {max_num}")
+    print(f"You have {max_attempts} attempts to guess it.")
+    print("-"*40)
+
+    # Game loop
+    attempts = 0
+    guessed_correctly = False
+
+    while attempts < max_attempts and not guessed_correctly:
+        attempts += 1
 
 number_guessing_game()
