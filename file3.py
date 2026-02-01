b3be0511fa22948e043d49d2c3a8eaca3425127f
abdullah97-pro <abdullahahmadi019@gmail.com>
2026-02-01 14:00:59 +0430

    class and objects

---
+++ file3.py
@@ -0,0 +1,47 @@
+# # # class Dog:
+# # #     def bark(self):
+# # #         print("Woof")
+
+# # # # object
+# # # myObj = Dog()
+# # # myObj.bark()
+
+# # class Vehical:
+# #     def Toyota(self):
+# #         print("4Runner!")
+
+# # myCar = Vehical()
+# # myCar.Toyota()
+
+
+# # class Dog:
+# #     def __init__(self, name, age):
+# #         self.name = name        # Instance attribute
+# #         self.age = age
+
+# #     def bark(self):
+# #         print(f"{self.name} says Woof!")
+
+# # my_dog = Dog("Buddy", 3)
+# # my_dog.bark()  
+
+
+# class Vehical:
+#     def __init__(self,name,model):
+#         self.name = name
+#         self.model = model
+
+#     def Car(self):
+#         print(f"{self.name} model {self.model}")
+
+# myCar = Vehical("Landcruiser","2025")
+# myCar.Car()
+
+
+class Person:
+    def greet(self):
+        print("Welcome to OOP")
+
+obj = Person()
+obj.greet()
+
